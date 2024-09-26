d65f5a36423d6e975fcb232284a5e0abb30bdf32
Ashank Rajendran <ashankrajendran@gmail.com>
2024-09-26 15:44:32 -0400

    Added Static Helpers Program

---
+++ Demos/Test/Test/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] words = new string[5];
+            //foreach (string word in words)
+            //{
+            //    Console.WriteLine(word);
+            //    //Console.WriteLine(word.Length);
+            //}
+
+            //bool[] boolValues = new bool[5];
+            //foreach (bool value in boolValues)
+            //{
+            //    Console.WriteLine(value);
+            //}
+
+            char[] chars = new char[5];
+            foreach(char value in chars)
+            {
+                Console.WriteLine(value);
+            }
+
+            //int[] numbers = new int[10];
+            //Console.WriteLine(numbers[90]);     //index out of range error
+        }
+    }
+}
---
+++ PEs/PE_StaticHelperMethods_AshankRajendran/PE_StaticHelperMethods_AshankRajendran/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace PE_StaticHelperMethods_AshankRajendran
+{
+    internal class Program
+    {
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // LEAVE THE REST OF THE CODE ALONE!
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        /// <summary>
+        /// Helper written by Prof. Mesh
+        /// Check if one number is a factor of another value
+        /// </summary>
+        /// <param name="factor">The factor to test</param>
+        /// <param name="value">The value to check</param>
+        /// <returns>True if value can be evenly divided by the factor</returns>
+        public static bool IsFactorOf(int factor, int value)
+        {
+            // Return true if "factor" is smaller than "value"
+            // and is evenly divisible into "value"
+            return factor < value && value % factor == 0;
+        }
+
+
+        /// <summary>
+        /// Input helper written by Prof. Mesh
+        /// Uses the given string to prompt the user for input and set
+        /// the color to cyan while they type.
+        /// </summary>
+        /// <param name="prompt">What to print before waiting for input</param>
+        /// <returns>A trimmed version of what the user entered</returns>
+        public static string GetPromptedInput(string prompt)
+        {
+            // Always print in white
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Print the prompt
+            Console.Write(prompt + " ");
+
+            // Switch color and get user input (trim too)
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string response = Console.ReadLine().Trim();
+
+            // Switch back to white and then return response.
+            Console.ForegroundColor = ConsoleColor.White;
+            return response;
+        }
+
+        /// <summary>
+        /// Input helper written by Ashank
+        /// Check if either number is a factor of the other and
+        /// print an appropriate response for both cases
+        /// </summary>
+        /// <param name="a">The first integer number</param>
+        /// <param name="b">The second integer number</param>
+        /// <returns>void</returns>
+        public static void CheckNumbers(int a, int b)
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Setup variables
+            string name = "";
+            int a = 0;
+            int b = 0;
+            int choice = 0;
+
+            // Get values for name, a, and b using GetPromptedInput and parsing if needed.
+            // Fyi, lines that begin with // TODO: will appear in a VS task list for you!
+            // https://docs.microsoft.com/en-us/visualstudio/ide/using-the-task-list
+            // TODO: ADD YOUR USER INPUT CODE HERE
+
+            // Reformat the name
+            name = name[0].ToString().ToUpper() + name.Substring(1, name.Length - 1).ToLower();
+
+            // Print the menu
+            Console.WriteLine("\nHello {0}, what would you like to do?\n" +
+                "\t1 - Compare numbers\n" +
+                "\t2 - Get my secret code\n" +
+                "\t3 - Output all info",
+                name);
+            choice = int.Parse(GetPromptedInput(">"));
+            Console.WriteLine();
+
+            // Figure out what to do and do it
+            switch (choice)
+            {
+                // Check numbers
+                case 1:
+                    // TODO: ADD YOUR CODE HERE
+                    break;
+
+                // Get secret code
+                case 2:
+                    // TODO: ADD YOUR CODE HERE
+                    break;
+
+                // Output all info
+                case 3:
+                    // TODO: ADD YOUR CODE HERE
+                    break;
+
+                // Say goodbye for invalid choices
+                default:
+                    Console.WriteLine("That wasn't a valid choice. Goodbye.");
+                    break;
+            }
+
+        }
+    }
+}
