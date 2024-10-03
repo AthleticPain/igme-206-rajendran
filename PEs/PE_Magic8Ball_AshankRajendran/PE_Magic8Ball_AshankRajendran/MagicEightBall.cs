adeaadaa09ca290399cd7643034120b5c004e798
Ashank Rajendran <ashankrajendran@gmail.com>
2024-10-02 21:04:38 -0400

    Uploading Magic Eight Ball PE

---
+++ PEs/PE_Magic8Ball_AshankRajendran/PE_Magic8Ball_AshankRajendran/MagicEightBall.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace PE_Magic8Ball_AshankRajendran
+{
+    internal class MagicEightBall
+    {
+        //Declare all variables for this scope
+        string owner;
+        int timesShaken;
+        string[] potentialResponses;
+        Random randomObject;
+
+        //Method to return random response from potentialResponses
+        //Also increments timesShaken
+        public string ShakeBall()
+        {
+            timesShaken++;
+
+            //Get a random index value
+            int randomIndex = randomObject.Next(0, potentialResponses.Length);
+            return potentialResponses[randomIndex];
+        }
+
+        //Method to return a statement about the number of times
+        //the eight ball has been shaken
+        public string Report()
+        {
+            if(timesShaken == 0)
+            {
+                return owner + " has not shaken the ball yet.";
+            }
+            else if(timesShaken >= 1 && timesShaken <= 3)
+            {
+                return string.Format("{0} has shaken the ball {1} times.",
+                    owner,
+                    timesShaken);
+            }
+            else //If timesShaken > 4
+            {
+                return string.Format("{0} has shaken the ball {1} times. " +
+                    "That's a lot of questions!",
+                    owner,
+                    timesShaken);
+            }
+
+        }
+
+        //Constructor to initialize instance of this class
+        public MagicEightBall(string ownerName) 
+        { 
+            this.owner = ownerName;
+            timesShaken = 0;
+            potentialResponses = new string[]
+            {
+                //Positive Responses
+                "Yes, Nostradamus predicted it would be so.",
+                "You can bet your life savings on it!",
+                "Yes, but only if you take decisive action right now!",
+                "Yes, although there may be an unexpected twist.",
+                "Yes, it was foretold in ancient prophecies.",
+                "Much like death and taxes, it is certain.",
+
+                //Neutral Responses
+                "I think you know the answer to that already.",
+                "I am not at liberty to divulge this information.",
+                "The movie 'Shrek' may have the answer you seek.",
+                "If I told you the answer, it may drastically change the course of the future.",
+                "Trust me buddy, you don't wanna know the answer to that.",
+                "You want the truth? You can't handle the truth!",
+
+                //Negative Responses
+                "That ain't happening.",
+                "I have personally taken steps to ensure that the answer to this is 'No'.",
+                "Never in a million years.",
+                "No, and I mean no. Don't ask me this again.",
+                "Thank you for expressing your interest in this. " +
+                "Unfortunately, my answer this time will have to be 'No'.",
+                "For your own good, the answer is 'No'."
+            };
+
+            randomObject = new Random();
+        }
+    }
+}
---
+++ PEs/PE_Magic8Ball_AshankRajendran/PE_Magic8Ball_AshankRajendran/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace PE_Magic8Ball_AshankRajendran
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare all variables for this scope here
+            string userChoice = "";
+
+            //Print welcome statement
+            Console.WriteLine("Welcome to Sassy 8 Ball Simulator!");
+
+            //Ask user for owner name
+            Console.Write("\nWho owns this Sassy 8 Ball? ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            //Create a MagicEightBall object with given input owner name
+            MagicEightBall eightBall = new MagicEightBall(Console.ReadLine().Trim());
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //While loop to keep asking user for a choice
+            while (userChoice != "quit")
+            {
+                //Ask user to input their choice
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.Write("You can 'shake' the ball, " +
+                    "get a 'report', " +
+                    "or 'quit': ");
+                
+                Console.ForegroundColor = ConsoleColor.Blue;
+                userChoice = Console.ReadLine().Trim().ToLower();
+                Console.ForegroundColor = ConsoleColor.White;
+
+                //Switch statement for what to do depending on userChoice
+                switch(userChoice)
+                {
+                    case "shake":
+
+                        //Prompt user for their question
+                        Console.Write("What is your question? ");
+                        Console.ForegroundColor= ConsoleColor.Blue;
+                        Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        Console.Write("The Sassy 8 Ball says: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine(eightBall.ShakeBall());
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        break;
+                    
+                    case "report":
+                        Console.WriteLine(eightBall.Report());
+                        break;
+
+                    case "quit":
+                        //Loop ends after this
+                        Console.WriteLine("Goodbye!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                }
+            }
+
+        }
+    }
+}
