e3fa382ab9d978b2c2153d5659e0b5da114a4df3
Ashank Rajendran <ashankrajendran@gmail.com>
2024-10-22 16:41:30 -0400

    Snowman Unity Project

---
+++ UnityProjects/My project/Assets/Scripts/AddForce.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+public class AddForce : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Vector3 forceDirection = Vector3.forward;
+    public float forceMagnitude = 10f;
+
+    void Start()
+    {
+        // Add force to the Rigidbody when the script starts
+        rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+    }
+}
