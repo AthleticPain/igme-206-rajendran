0698ec988ace43eb8deffb4490678a76c9e39009
Ashank Rajendran <ashankrajendran@gmail.com>
2024-11-06 22:28:55 -0500

    Added Wrap Feature

--- UnityProjects/Projects/PE U4/Assets/Scripts/MovementController.cs
+++ UnityProjects/Projects/PE U4/Assets/Scripts/MovementController.cs
@@ -8,7 +8,7 @@ public class MovementController : MonoBehaviour
     Vector3 objectPosition = new Vector3(0, 0, 0);
 
     // How fast it should move in units per second
-    float speed = 4f;
+    [SerializeField] float speed = 4f;
 
     // Direction vehicle is facing, must be normalized
     Vector3 direction = new Vector3(0, 0, 0);   // or Vector3.zero
@@ -16,12 +16,37 @@ public class MovementController : MonoBehaviour
     // The delta in position for a single frame
     Vector3 velocity = new Vector3(0, 0, 0);   // or Vector3.zero
 
+    // Variable that stores the camera gameobject
+    private Camera cam;
+
+    //upper and lower bounds of screen in world space
+    public float maxWidth;
+    public float minWidth;
+    public float maxHeight;
+    public float minHeight;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Assign the scene's main camera to cam variable
+        cam = Camera.main;
+
         // Grab the GameObject’s starting position
         objectPosition = transform.position;
 
+    }
+
+    private void OnGUI()
+    {
+        //Convert the screen space lower and upper bounds to world space
+        Vector3 lowerBounds = cam.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 upperBounds = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, 0));
+
+        //set lower and upper bound variables
+        maxWidth = upperBounds.x;
+        maxHeight = upperBounds.y;
+        minWidth = lowerBounds.x;
+        minHeight = lowerBounds.y;
     }
 
     // Update is called once per frame
@@ -38,6 +63,24 @@ public class MovementController : MonoBehaviour
         // “Draw” this vehicle at that position
         transform.position = objectPosition;
 
+        //Check if player has gone beyond map bounds and wrap them if so
+        if (transform.position.x > maxWidth)
+        {
+            objectPosition = new Vector3(minWidth, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x < minWidth)
+        {
+            objectPosition = new Vector3(maxWidth, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.y > maxHeight)
+        {
+            objectPosition = new Vector3(transform.position.x, minHeight, transform.position.z);
+        }
+        else if (transform.position.y < minHeight)
+        {
+            objectPosition = new Vector3(transform.position.x, maxHeight, transform.position.z);
+        }
+
         // Set the vehicle’s rotation to match the direction
         if (direction != Vector3.zero)
         {
@@ -48,5 +91,10 @@ public class MovementController : MonoBehaviour
     public void SetDirection(Vector2 inputDirection)
     {
         direction = inputDirection;
+    }
+
+    public void SetPosition(Vector2 newPosition)
+    {
+        transform.position = newPosition;
     }
 }
---
+++ UnityProjects/Projects/PE U4/ReadMe.md
@@ -0,0 +1,24 @@
+  
+**Game Project**:  PE U4 - Vehicle
+
+**Creator**:  Ashank Rajendran
+
+**Version**:   0.1
+
+**Date**: 11/06/2024
+
+**New features**:
+2D vehicle that moves with player input using vector movement
+Vehicles wraps when leaving camera bounds
+
+﻿  
+**Fixes**:
+None
+﻿  
+**Improvements** *(of existing features)*:
+None
+
+**Known Bugs**:
+None
+
+﻿  
